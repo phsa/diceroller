081d15d704e379eace1e1b40e8cb8a856142547e
phsa <phsaraujo54@gmail.com>
2020-10-07 21:51:27 -0300

    Customization for roll button

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -19,12 +19,12 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val properResource = when ((Random().nextInt(6) + 1)) {
-            1 -> R.drawable.image_dice_1
-            2 -> R.drawable.image_dice_2
-            3 -> R.drawable.image_dice_3
-            4 -> R.drawable.image_dice_4
-            5 -> R.drawable.image_dice_5
-            else -> R.drawable.image_dice_6
+            1 -> R.drawable.icon_main_dice_1
+            2 -> R.drawable.icon_main_dice_2
+            3 -> R.drawable.icon_main_dice_3
+            4 -> R.drawable.icon_main_dice_4
+            5 -> R.drawable.icon_main_dice_5
+            else -> R.drawable.icon_main_dice_6
         }
         viewBinding.imageDice.setImageResource(properResource)
     }
