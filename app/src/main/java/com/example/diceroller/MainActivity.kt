48fdc19b38bd70d74393575d0a30e303343d2523
phsa <phsaraujo54@gmail.com>
2020-10-07 21:09:20 -0300

    Fininshing DiceRoller class

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -18,7 +18,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val result = (Random().nextInt(6) + 1)
-        viewBinding.textResult.text = result.toString()
+        val properResource = when ((Random().nextInt(6) + 1)) {
+            1 -> R.drawable.image_dice_1
+            2 -> R.drawable.image_dice_2
+            3 -> R.drawable.image_dice_3
+            4 -> R.drawable.image_dice_4
+            5 -> R.drawable.image_dice_5
+            else -> R.drawable.image_dice_6
+        }
+        viewBinding.imageDice.setImageResource(properResource)
     }
 }
